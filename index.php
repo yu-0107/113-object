fd83084ed38755ea0d47af0a6d8d9468dc4438f3
onion <a0966550619@gmail.com>
2024-11-22 13:40:06 +0800

    物件設計與練習

--- index.php
+++ index.php
@@ -12,40 +12,44 @@
     <?php
     class Animal
     {
-        public $type='animal';
-        public $name='John';
-        public $hair_color='black';
-
-        function __construct($type, $name, $hair_color)
-        {
+        protected $type='animal';
+        protected $name='John';
+        protected $hair_color='black';
+        protected $feet=['font-left','font-right','backleft','backright'];
+        
+        function __construct($type, $name, $hair_color){
             $this->type = $type;
             $this->name = $name;
             $this->hair_color = $hair_color;
         }
 
-        function run()
-        {
+        function run(){
             echo $this->name . 'is running';
         }
 
-        public function speed()
-        {
+        public function speed(){
             echo $this->name . 'is running at 20km/h';
+        }
+        public function getName(){
+            return $this->name;
+        }
+        public function setName($name){
+             $this->name=$name;
         }
     }
 
     //實力化(instance)
     $cat = new Animal('cat', 'kitty', 'white');
 
-    echo $cat->type;
-    echo $cat->name;
-    echo $cat->hair_color;
+    // echo $cat->type;
+    echo $cat->getName();
+    // echo $cat->hair_color;
     echo $cat->run();
     echo $cat->speed();
-    
+    // print_r($cat->feet);
 
-
-
+    $cat->setName('Mary');
+    echo $cat->getName();
 
 
 
