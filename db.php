11d35552f7ca9a6f1e9afdec3a4526885943e0d3
onion <a0966550619@gmail.com>
2024-11-26 16:26:42 +0800

    完成save()函式

--- db.php
+++ db.php
@@ -9,11 +9,12 @@ class DB{
         $this->table=$table;
         $this->pdo=new PDO($this->dsn,'root','');
     }
+
     /**
      * 撈出全部資料
-     * 1.整理資料表
-     * 2.有條件
-     * 3.其他SQL功能
+     * 1. 整張資料表
+     * 2. 有條件
+     * 3. 其他SQL功能
      */
     function all(...$arg){
         $sql="SELECT * FROM $this->table ";
@@ -23,10 +24,12 @@ class DB{
                 $where=$this->a2s($arg[0]);
                 $sql=$sql . " WHERE ". join(" && ",$where);
             }else{
-                // $sal=$sql.$arg[0];
+                //$sql=$sql.$arg[0];
                 $sql .= $arg[0];
+
             }
         }
+
         if(!empty($arg[1])){
             $sql=$sql . $arg[1];
         }
@@ -45,6 +48,26 @@ class DB{
         }
         return $this->fetchOne($sql);
     }
+
+    function save($array){
+
+        if(isset($array['id'])){
+            //update
+            //update table set `欄位1`='值1',`欄位2`='值2' where `id`='值' 
+            $id=$array['id'];
+            unset($array['id']);
+            $set=$this->a2s($array);
+            $sql ="UPDATE $this->table SET ".join(',',$set)." where `id`='$id'";
+                
+        }else{
+            //insert
+            $cols=array_keys($array);
+            $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+        }
+        
+        echo $sql;
+        return $this->pdo->exec($sql);
+    }
     
     function del($id){
         $sql="DELETE FROM $this->table ";
@@ -56,9 +79,11 @@ class DB{
             $sql .= " WHERE `id`='$id' ";
         }
 
-        echo $sql;
+        //echo $sql;  
         return $this->pdo->exec($sql);
     }
+
+    
     /**
      * 把陣列轉成條件字串陣列
      */
@@ -70,19 +95,22 @@ class DB{
         return $tmp;
     }
 
+
     function fetchOne($sql){
-        //echo $sql
+        //echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
+    
     function fetchAll($sql){
-        //echo $sql
+        //echo $sql;
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
+    
 }
 
-// function q($sql){
-//     return $this->pdo->query($sql)->fetchAll();
+/* function q($sql){
+    return $this->pdo->query($sql)->fetchAll();
+} */
 
 function dd($array){
     echo "<pre>";
@@ -94,8 +122,11 @@ function dd($array){
 $DEPT=new DB('dept');
 
 // $dept=$DEPT->q("SELECT * FROM dept");
+// $dept=$DEPT->all(['id'=>3]);
+// $dept=$DEPT->find(2);
 $dept=$DEPT->find(['code'=>'404']);
-$DEPT->del(['code'=>'404']);
-
+// $DEPT->save(['code'=>'504']);
+$DEPT->save(['code'=>'504','id'=>'7','name'=>'資訊發展部']);
+// $DEPT->del(2);
+// $DEPT->del(['code'=>'404']);
 dd($dept);
-
